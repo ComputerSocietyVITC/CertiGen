cc83ec9ba9911b9943c4e7fc6f1936979d389b49
Manav Garg <58691392+ManavvGarg@users.noreply.github.com>
2023-06-12 21:38:54 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -80,7 +80,7 @@ function App() {
 
       formData.append('font_size', fontSize);
 
-      const response = await axios.post('https://certi-gen-api.onrender.com/process', formData, {
+      const response = await axios.post('https://certi-gen-v5kq3.ondigitalocean.app', formData, {
         headers: { 'Content-Type': 'multipart/form-data', 'Access-Control-Allow-Origin': '*' },
         responseType: 'blob',
       });
